0e721e023c795bf6c3a54606273c25e764c506b5
emmycode53 <dope98577@gmail.com>
2025-10-01 19:11:55 +0100

    fix:: cors added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const cors = require('cors')
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -10,12 +10,13 @@ const paymentroutes = require('./routes/paymentroutes');
 const app = express()
 
 
-
+app.use(cors())
 
 
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.json());
+
 
 app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
