c7219be19d5f14ec5bbe31c29c57a8568d3c1f45
emmycode53 <dope98577@gmail.com>
2025-10-02 14:52:56 +0100

    fixed:: route

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ require('dotenv').config();
 const {swaggerUi, swaggerSpec} = require('./swagger')
 const authroutes = require('./routes/usersroutes');
 const requestroutes = require('./routes/requestroutes');
-const paymentroutes = require('./routes/paymentroutes');
-
+const webhookroute = require('./routes/webhookrout');
+const initializePaymentroute = require('./routes/initializepaymentroute')
 const app = express()
 
 
 app.use(cors({origin: '*'}))
 
+app.use('/payment', webhookroute);
 
 app.use(express.urlencoded({extended:true}));
 
@@ -20,10 +21,10 @@ app.use(express.json());
 
 
 app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+app.use('/', initializePaymentroute)
 app.use('/auth', authroutes);
 app.use('/request', requestroutes);
-app.use('/payment', paymentroutes);
+
  
 mongoose.connect(process.env.MONGO_ATLAS_URI)
 .then(()=> console.log('connected to the database'))
--- routes/initializepaymentroute.js
+++ routes/initializepaymentroute.js
@@ -1,8 +1,7 @@
-
-const paystack = require('../controller/paymentcontroller');
-const authenticateUser = require('../services/authenticateUser');
 const express = require('express');
 const router = express.Router();
+const payment = require('../controller/paymentcontroller');
+const authenticateUser = require('../services/authenticateUser');
 
 /**
  * @swagger
@@ -80,58 +79,4 @@ const router = express.Router();
  *       scheme: bearer
  *       bearerFormat: JWT
  */
-router.post('/initialize', authenticateUser, paystack.initializePayment);
-
-/**
- * @swagger
- * /payment/webhook:
- *   post:
- *     summary: Paystack webhook endpoint
- *     description: Handles Paystack payment events such as "charge.success". Updates payment status in the database.
- *     tags:
- *       - Payment
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             description: Webhook payload sent by Paystack
- *             properties:
- *               event:
- *                 type: string
- *                 example: "charge.success"
- *               
- *                       
- *                       
- *                         
- *                        
- *     responses:
- *       200:
- *         description: Webhook processed successfully
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: "OK"
- *       401:
- *         description: Invalid signature
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "invalid signature"
- *       500:
- *         description: Internal server error
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: "Internal server error"
- */
-router.post('/webhook',express.raw({type:'application/json'}), paystack.paystackWebhook);
-
-module.exports = router;
+router.post('initialize' ,authenticateUser, payment.initializePayment);
